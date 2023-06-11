9a2d9bf6695d2af13c9192ead4729fd343c7e57a
nbmLon99 <rhfwleowkd77@gmail.com>
2023-06-12 02:00:32 +0900

    기온 받아오는 거 성공 날씨 : PTY 기준으로 해서 1을 찾지못함 SKY로 하려고 예보데이터 써봤는데 복잡할 뿐더러 PTY기준으로 해도 손색없어 보여서 다시 돌렸습니다.

--- map.js
+++ map.js
@@ -1,10 +1,18 @@
+document.write('<script src="weather.js"></script>');
+
 const markerImageSize = new kakao.maps.Size(24, 35);
 const markerImage = new kakao.maps.MarkerImage("icons/marker.png", markerImageSize);
 
 let map = null;
 let places = null;
 
+let weatherImg_ = null;
+let weatherText_ = null;
+
 function generateMap(lat, lng) {
+  const weatherImg = document.getElementById('weatherImg');
+  const weatherText = document.getElementById('weatherText');
+
   console.log("map generate: ", lat, lng);
   let mapContainer = document.getElementById("map");
   let mapOption = {
@@ -14,6 +22,8 @@ function generateMap(lat, lng) {
 
   map = new kakao.maps.Map(mapContainer, mapOption);
   places = new kakao.maps.services.Places();
+
+  setElement(weatherImg,weatherText);
 }
 
 function generateMarker(lat, lng) {
@@ -25,6 +35,7 @@ function generateMarker(lat, lng) {
     image: markerImage,
   });
 }
+
 
 function search(keyword) {
   places.keywordSearch(keyword, searchCallback);
@@ -40,4 +51,16 @@ function searchCallback(data, status, pagination) {
     alert("검색 결과 중 오류가 발생했습니다.");
     return;
   }
+}
+
+function setElement(weatherImg, weatherText){
+  kakao.maps.event.addListener(map, 'dragend', function() {        
+    // 지도 중심좌표를 얻어옵니다 
+    var latlng = map.getCenter(); 
+    getWeather(latlng.getLat(),latlng.getLng(),weatherImg,weatherText);
+    var message = '변경된 지도 중심좌표는 ' + latlng.getLat() + ' 이고, ';
+    message += '경도는 ' + latlng.getLng() + ' 입니다';
+
+    console.log(message);
+});
 }

46b07e1b50b3b26e72e7d63d163af81afb319e57
이문빈 <3412mb@gmail.com>
2023-06-19 23:25:41 +0900

    Update shelter.js

--- js/shelter.js
+++ js/shelter.js
@@ -1,16 +1,15 @@
 const base = "https://apis.data.go.kr/1741000/HeatWaveShelter2/getHeatWaveShelterList2?";
-const serviceKey = 
-  "Ra8KnDXQFqkPHWYJuYj5WIo0zbXUNa%2BIs8938WOplm3Y5s81nwmWLRbeCO5yvf29u5CQPiSv2tU%2FEFSe8alD5Q%3D%3D";
+const serviceKey = "Ra8KnDXQFqkPHWYJuYj5WIo0zbXUNa%2BIs8938WOplm3Y5s81nwmWLRbeCO5yvf29u5CQPiSv2tU%2FEFSe8alD5Q%3D%3D";
 const serviceKey2 =
   "%2FPyXD53d13dDxi79kkPHb%2BqLRlHbhwyyM7fFVHAUMVAm%2F2UI8TbN%2BolzR10R2weyurRdimRWgXeCGt1Tdos%2B%2FQ%3D%3D";
 const numOfRows = "1000";
 const type = "json";
 const year = "2023";
 
-const locationBase = "https://apis.data.go.kr/1741000/StanReginCd/getStanReginCdList?serviceKey=Ra8KnDXQFqkPHWYJuYj5WIo0zbXUNa%2BIs8938WOplm3Y5s81nwmWLRbeCO5yvf29u5CQPiSv2tU%2FEFSe8alD5Q%3D%3D&pageNo=1&numOfRows=1&type=json&locatadd_nm="
+const locationBase =
+  "https://apis.data.go.kr/1741000/StanReginCd/getStanReginCdList?serviceKey=Ra8KnDXQFqkPHWYJuYj5WIo0zbXUNa%2BIs8938WOplm3Y5s81nwmWLRbeCO5yvf29u5CQPiSv2tU%2FEFSe8alD5Q%3D%3D&pageNo=1&numOfRows=1&type=json&locatadd_nm=";
 
 let allShelters = [];
-
 
 function getShelterInfo() {
   // 나중에 62로 설정해야함
@@ -35,26 +34,26 @@ function getShelterInfo() {
         shelters.forEach((shelter) => {
           allShelters.push(shelter);
         });
-      }).catch(error => {
+      })
+      .catch((error) => {
         // 네트워크 오류나 다른 예외를 처리하는 로직
-        console.log('Error:', error);
+        console.log("Error:", error);
       });
   }
 }
 
 //반경 1km 쉘터만 표시
-function setNearbyShelters(lat,lon){
-  var results = []
+function setNearbyShelters(lat, lon) {
+  var results = [];
 
-  allShelters.forEach((sht)=>{
-    var distance = calculateDistance(sht.la, sht.lo, lat, lon)
-    if(distance <= 1){
-      results.push(sht)
+  allShelters.forEach((sht) => {
+    var distance = calculateDistance(sht.la, sht.lo, lat, lon);
+    if (distance <= 1) {
+      results.push(sht);
     }
-  })
-  generateShelterMarker(results)
+  });
+  generateShelterMarker(results);
 }
-
 
 //지도 상 거리 계산하는 함수
 function calculateDistance(lat1, lon1, lat2, lon2) {
@@ -69,8 +68,7 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
 
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
-    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
 
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
@@ -80,42 +78,42 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
 
 // 검색결과 항목을 Element로 반환하는 함수입니다
 function getListShelter(shelter) {
-  var el = document.createElement("li")
+  var el = document.createElement("li");
 
   switch (String(shelter.equptype)) {
     //노인시설
     case "001":
-      marker_color = "r"
+      marker_color = "r";
       break;
 
     //복지회관 / 마을회관
-    case "002" :
-    case "003" :
-      marker_color = "g"
+    case "002":
+    case "003":
+      marker_color = "g";
       break;
 
     //공원 / 정자
     case "009":
     case "010":
-      marker_color = "b"
+      marker_color = "b";
       break;
 
     //기타
     default:
-      marker_color = "y"
+      marker_color = "y";
   }
 
   itemStr =
-      '<span class="marker_sht marker_' +
-      marker_color +
-      '"></span>' +
-      '<div class="info">' +
-      "   <h5>" +
-      shelter.restname +
-      "</h5>";
-  itemStr += "    <span>" + (shelter.restaddr) + "</span>"
-  itemStr += '  <span class="tel">' + (shelter.mngdptCd) + "</span>" + "</div>";
-  
+    '<span class="marker_sht marker_' +
+    marker_color +
+    '"></span>' +
+    '<div class="info">' +
+    "   <h5>" +
+    shelter.restname +
+    "</h5>";
+  itemStr += "    <span>" + shelter.restaddr + "</span>";
+  itemStr += '  <span class="tel">' + shelter.mngdptCd + "</span>" + "</div>";
+
   el.innerHTML = itemStr;
   el.className = "item";
 
